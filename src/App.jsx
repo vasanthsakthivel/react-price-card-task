f829552f5afe8c1decebda50a518d69e8dd2def9
vasanthsakthivel <s.vasanth@live.com>
2024-09-17 12:01:09 +0530

    adding app components

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,13 @@
-import Card from './components/Card.jsx';
-import './App.css';
+import TodoForm from "./components/TodoForm"
 
 
-
-function App() {
- const price = [
-  {
-    title: "FREE",
-    price: "0/month",
-    feature: [
-      { name: "Single User", enabled: true},
-      { name: "50GB Storage", enabled: true},
-      { name: "Unlimited Public Projects", enabled: true},
-      { name: "Community Access", enabled: true},
-      { name: "Unlimited Private Projects", enabled: false},
-      { name: "Dedicated Phone Support", enabled: false},
-      { name: "Free subdomain", enabled: false},
-      { name: "Monthly Status Report", enabled: false},
-    ],
-    buttonlabel: "BUTTON"
-  },
-  {
-    title: "PLUS",
-    price: "$9/month",
-    feature: [
-      { name: "5 Users", enabled: true},
-      { name: "50GB Storage", enabled: true},
-      { name: "Unlimited Public Projects", enabled: true},
-      { name: "Community Access", enabled: true},
-      { name: "Unlimited Private Projects", enabled: true},
-      { name: "Dedicated Phone Support", enabled: true},
-      { name: "Free subdomain", enabled: true},
-      { name: "Monthly Status Report", enabled: false},
-    ],
-    buttonlabel: "BUTTON"
-  },
-  {
-    title: "PLUS",
-    price: "$49/month",
-    feature: [
-      { name: "5 Users", enabled: true},
-      { name: "50GB Storage", enabled: true},
-      { name: "Unlimited Public Projects", enabled: true},
-      { name: "Community Access", enabled: true},
-      { name: "Unlimited Private Projects", enabled: true},
-      { name: "Dedicated Phone Support", enabled: true},
-      { name: "Free subdomain", enabled: true},
-      { name: "Monthly Status Report", enabled: true},
-    ],
-    buttonlabel: "BUTTON"
-  },
- ];
-
+const App = () => {
   return (
-    <div className="App">
-      <div className='pricing-container'>
-        {price.map((option, index) => (
-          <Card 
-          key={index}
-          title={option.title}
-          price={option.price}
-          feature={option.feature}
-          buttonlabel={option.buttonlabel}
-          />
-        ))
-        }
-      </div>
+    <div className="app-container">
+      <h2>My Todo App</h2>
+      <TodoForm />
     </div>
-  );
+  )
 }
-export default App;
 
+export default App
---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,21 @@
+
+
+const TodoForm = () => {
+  return (
+   <form>
+    <input 
+    type="text"
+    placeholder="Todo Name" 
+
+     />
+     <input 
+     type="text" 
+     placeholder="Todo Description"
+
+      />
+      <button>Add Todo</button>
+   </form>
+  )
+}
+
+export default TodoForm
